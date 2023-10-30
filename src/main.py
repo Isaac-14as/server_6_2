817f303824537f9fbc373640501dd1809329ddb3
Дмитрий Еланский <elan.dmitriy@gmail.com>
2023-10-30 19:52:50 +0300

    v_2_other_21

--- src/main.py
+++ src/main.py
@@ -41,9 +41,9 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""], # Allows all origins
+    allow_origins=["*"], # Allows all origins
     allow_credentials=True,
-    allow_methods=[""], # Allows all methods
+    allow_methods=["*"], # Allows all methods
     allow_headers=["*"], # Allows all headers
 )
 
