f0aa4c7e05d8af76bfff4a6f4d09824ed9d3a061
Дмитрий Еланский <elan.dmitriy@gmail.com>
2023-10-30 20:07:43 +0300

    v_2_other_23

--- src/main.py
+++ src/main.py
@@ -39,14 +39,28 @@ origins = [
 #     expose_headers=["*"],
 # )
 
+# origins = ["*"]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://client-i54l.onrender.com"], # Allows all origins
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"], # Allows all methods
-    allow_headers=["Origin", "Authorization"], # Allows all headers
-    expose_headers=["Content-Length"],
+    allow_origins=['*']
 )
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["https://client-i54l.onrender.com"], # Allows all origins
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"], # Allows all methods
+#     allow_headers=["Origin", "Authorization"], # Allows all headers
+#     expose_headers=["Content-Length"],
+# )
 
 
 app.include_router(
